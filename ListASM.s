39a65599e8745c5a1a02e2cc558e6faab1a7336c
Andrew Waugh <awaugh@pdx.edu>
2016-05-09 09:48:57 -0700

    Fixes the return of additem to no longer return to a point in the stack. Fixes an unitialized pointer jump.

--- ListASM.s
+++ ListASM.s
@@ -30,6 +30,7 @@ addToList:
 
 	movq %rax, (%rdi)	#Set our first pointer to point to the new node.
 	movq (%rdi), %rdi 	#Get the actual struct starting address
+	movq $0, (%rdi)		#Initialize a null next pointer
 	movl %esi, 8(%rdi)	#Fill in the data at the integer loc for the struct.
 
 .addToList_End:
@@ -40,33 +41,41 @@ addToList:
 
 addItem:
 
-	pushq %rbp 	#Push base poitner onto stack
-	movq %rsp, %rbp	#Move previous stack pointer to new base
-	subq $8,%rsp #Allocate space for local variables
+	pushq %rbp 		#Push base poitner onto stack
+	movq %rsp, %rbp		#Move previous stack pointer to new base
+	subq $8,%rsp 		#Allocate space for local variables
 
-	movq $0, %rax #clear rax
+	movq $0, %rax 		#clear rax
 	movq %rdi,-8(%rsp)	#Store the current node pointer on the stack
-	cmpq $0, (%rdi) 		#If head->next is null
-	je .addItem_end 
+	cmpq $0, (%rdi) 	#If head->next is null
+	je .addItem_add 
 	#Since the next pointer is the first element in the node struct,
 	#we can simply dereference the pointer and store it in itself to
 	#move our current forward.
-	movq (%rdi), %rdi
+	movq (%rdi), %rdi	#head = head->next
 	call addItem
+	jmp .addItem_end
 
-.addItem_end:
+.addItem_add:
 	
-	#Calling malloc is cheating, but we're starting small!
 	pushq %rdi
-	movq $16,%rdi 	#Size of data to allocate. Naievely assuming 16 bytes.
+	pushq %rsi
+
+	movq $16,%rdi 		#Size of data to allocate. Naievely assuming 16 bytes.
 	call malloc
+	popq %rsi
 	popq %rdi
 	movq %rax,(%rdi)	#Store the pointer returned by malloc into our cur->next
 	movl %esi, 8(%rdi)	#Store the data into the integer of our struct
 	movq (%rdi), %rdi 	#increment current so we can modify cur->next
 	movq $0, (%rdi)
 
+.addItem_end:
+
+	addq $8, %rsp
 	popq %rbp
 	ret
+
+	
 	
 	
--- listdriver.c
+++ listdriver.c
@@ -5,6 +5,8 @@ int main() {
 	List test = NULL;
 	initList(&test);
 	addToList(test, 5);
+	addToList(test, 10);
+	addToList(test, 30);
 	displayList(test);
 	return 0;
 }
